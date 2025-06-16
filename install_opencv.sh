27c5aa9cbfcec1d4a7e21fc35a7aa04c6126b3b5
Your Name <you@example.com>
2025-06-17 02:57:26 +0800

    fixed line

--- install_opencv.sh
+++ install_opencv.sh
@@ -35,11 +35,11 @@ echo "------------------------------------"
 echo "** Install requirement (1/4)"
 echo "------------------------------------"
 sudo apt-get update
-sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev
-sudo apt-get install -y libv4l-dev v4l-utils qv4l2
-sudo apt-get install -y curl
+sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev --allow-change-held-packages
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev --allow-change-held-packages
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev --allow-change-held-packages
+sudo apt-get install -y libv4l-dev v4l-utils qv4l2 --allow-change-held-packages
+sudo apt-get install -y curl --allow-change-held-packages
 
 
 echo "------------------------------------"
@@ -61,7 +61,7 @@ echo "------------------------------------"
 mkdir release
 cd release/
 # CUDA_ARCH_BIN 8.7 is AGX orin
-cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D CUDA_ARCH_BIN="7.2,8.7" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 # save a core
 make -j$(($(nproc) - 1))
 
